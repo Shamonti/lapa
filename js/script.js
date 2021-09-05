39a872811a4b6e0813ff30ec8f8cfcfb2708ab26
Shamonti <67649277+Shamonti@users.noreply.github.com>
2021-09-05 13:33:17 +0600

    commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 jQuery(document).ready(function () {
+  // Banner Slider
 
-
-
-    // Banner Slider
-    
-$('.catalog-wrapper').slick({
+  $('.catalog-wrapper').slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 3,
-    prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-right"></i></button>',
+    prevArrow:
+      '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-prev"><i class="fas fa-angle-right"></i></button>',
     responsive: [
       {
         breakpoint: 1024,
@@ -19,101 +18,112 @@ $('.catalog-wrapper').slick({
           slidesToShow: 4,
           slidesToScroll: 4,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
-        }
+          slidesToScroll: 3,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
         },
-      
-     
-     
-
-      }
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
-    ]
+    ],
   });
 
-//   Brand section
-function setDots(){
-  $(".owl-dots").removeClass('disabled');
-}
-$('.brand-wrapper').owlCarousel({
-  loop:true,
-  margin:10,
-  responsiveClass:true,
-  dots:true,
-  nav:false,
-  responsive:{
-      0:{
-          items:2,
-          nav:false
+  //   Brand section
+  function setDots() {
+    $('.owl-dots').removeClass('disabled');
+  }
+  $('.brand-wrapper').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 2,
+        nav: false,
       },
-      500:{
-          items:3,
-          nav:false
+      500: {
+        items: 3,
+        nav: false,
       },
-      992:{
-          items:4,
-          loop:true
-      }
-  },
-  onInitialized:setDots,
-  onChanged:setDots
-})
+      992: {
+        items: 4,
+        loop: true,
+      },
+    },
+    onInitialized: setDots,
+    onChanged: setDots,
+  });
 
-//   Item section
-$('.item-wrapper').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:false,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:3
-        },
-        992:{
-            items:4
-        },
-        1000:{
-            items:4
-        }
-    }
-})
+  //   Item section
+  $('.item-wrapper').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
 
-// Product zoom
+  // Product zoom
 
-// $('.zoom-img').picZoomer();
-$('.menu-img').on('click',function(event){
-
+  // $('.zoom-img').picZoomer();
+  $('.menu-img').on('click', function (event) {
     var $pic = $(this).find('img');
-    
-    $('.zoom-img img').attr('src',$pic.attr('src'));
-  
+
+    $('.zoom-img img').attr('src', $pic.attr('src'));
   });
-  
+});
 
+const modal = document.querySelector('.modal--signup');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal--signup');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
+overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
 
-
-
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
